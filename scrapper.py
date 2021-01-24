bd096aec82f543fbeeb7ae9dbec0e668c9697b69
jfpalomeque <jfpalomeque.gonzalez@gmail.com>
2021-01-24 00:09:01 +0000

    bs4 error

--- scrapper.py
+++ scrapper.py
@@ -3,7 +3,10 @@
 
 import requests
 import bs4
-from bs4 import BeautifulSoup
+try:
+    from bs4 import BeautifulSoup
+
+from bs4 import BeautifulSoup 
 import pandas as pd
 import time
 import streamlit as st
