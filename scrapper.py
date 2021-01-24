c272215c9f02f8ed35788b70a119684e7d3f2903
jfpalomeque <jfpalomeque.gonzalez@gmail.com>
2021-01-24 00:32:13 +0000

    typos

--- scrapper.py
+++ scrapper.py
@@ -132,8 +132,8 @@ def scrapper(title, location = "United Kingdom"):
 
 st.title("Welcome to the indeed.co.uk crapper")
 if st.button("See Readme"):
-    st.write("""This is an scrapper for Indeed.co.uk, the job ads website. This project has three parts, an advance webscrapper, a little exploratory analysis of those ads and a 
-    visualization tool using streamlite. Although sleepers where added in order to avoid IP ban by the website, some times the connection was blocked. 
+    st.write("""This is a scrapper for Indeed.co.uk, the job ads website. This project has three parts, an advance webscrapper, a little exploratory analysis of those ads and a 
+    visualization tool using streamlite. Although sleepers were added in order to avoid IP ban by the website, some times the connection was blocked. 
     I recomend use an VPN anc change server, or a similar solution, if it's going to be tried more than once in a short time period.
     The city name to coordinates translation is really slow too, making that the map take ages in appear, depending of the number of ads. Code in https://github.com/jfpalomeque/indeed_scrapper""")
 
